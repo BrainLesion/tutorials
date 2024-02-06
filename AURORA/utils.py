dbd4796ab7fa22a63e9e3f405a5a335a1ec68a47
Marcel Rosier <marcel.rosier@tum.de>
2024-02-06 15:38:13 +0100

    - fixed naming issue in vis

--- AURORA/utils.py
+++ AURORA/utils.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import nibabel as nib
 
 
-def visualize_results(t1_file: str, segmentation_file: str):
-    t1_np = nib.load(t1_file).get_fdata().transpose(2, 1, 0)
+def visualize_results(modality_file: str, segmentation_file: str):
+    modality_np = nib.load(modality_file).get_fdata().transpose(2, 1, 0)
     seg_np = nib.load(segmentation_file).get_fdata().transpose(2, 1, 0)
     _, ax = plt.subplots(1, 2, figsize=(8, 4))
 
-    slice_index = t1_np.shape[0] // 2  # You can choose any slice here
-    ax[0].imshow(t1_np[slice_index, :, :], cmap="gray")
-    ax[1].imshow(t1_np[slice_index, :, :], cmap="gray")
+    slice_index = modality_np.shape[0] // 2  # You can choose any slice here
+    ax[0].imshow(modality_np[slice_index, :, :], cmap="gray")
+    ax[1].imshow(modality_np[slice_index, :, :], cmap="gray")
     ax[1].imshow(seg_np[slice_index, :, :], cmap="plasma", alpha=0.3)
     for ax in ax:
         ax.axis("off")
