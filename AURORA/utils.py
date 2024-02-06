961e9b6b948d7879cd28650fd3ed7f8ded4e3cbd
Marcel Rosier <marcel.rosier@tum.de>
2024-02-06 16:25:33 +0100

    - add colab badge - clean up utils and nb, add explanations for colab - add general data vis - rename data files for consistency

--- AURORA/utils.py
+++ AURORA/utils.py
@@ -1,8 +1,29 @@
+import os
 import matplotlib.pyplot as plt
 import nibabel as nib
 
+DATA_FOLDER = "data"
 
-def visualize_results(modality_file: str, segmentation_file: str):
+
+def visualize_data(data_folder: str = DATA_FOLDER, slice_index: int = 75):
+    """Visualize the MRI modalities for a given slice index
+
+    Args:
+        data_folder (str, optional): Path to the folder containing the t1, t1c, t2 & flair file. Defaults to DATA_FOLDER.
+        slice_index (int, optional): Slice to be visualized (first index in data of shape (155, 240, 240)). Defaults to 75.
+    """
+    _, axes = plt.subplots(1, 4, figsize=(12, 10))
+
+    modalities = ["t1", "t1c", "t2", "flair"]
+    for i, mod in enumerate(modalities):
+        modality_file = os.path.join(data_folder, f"{mod}.nii.gz")
+        modality_np = nib.load(modality_file).get_fdata().transpose(2, 1, 0)
+        axes[i].set_title(mod)
+        axes[i].imshow(modality_np[slice_index, :, :], cmap="gray")
+        axes[i].axis("off")
+
+
+def visualize_segmentation(modality_file: str, segmentation_file: str):
     modality_np = nib.load(modality_file).get_fdata().transpose(2, 1, 0)
     seg_np = nib.load(segmentation_file).get_fdata().transpose(2, 1, 0)
     _, ax = plt.subplots(1, 2, figsize=(8, 4))
